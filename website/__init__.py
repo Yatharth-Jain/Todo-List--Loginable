343cacfa4566826dc21811fcf45511b7640645c9
Yatharth Jain <yatharthjain41@gmail.com>
2022-05-19 11:30:33 +0530

    Added Online DB

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from os import path
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager,current_user
 
 db=SQLAlchemy()
 DB_Name='Notes.db'
@@ -11,6 +11,7 @@ def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='My Secret'
     app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_Name}'
+    app.config['SQLALCHEMY_DATABASE_URI']='postgres://ngyhtwozmpmeqz:6f83a7f4a752601e9ec07e250a06bfbee185716fff17bc421150c31c76073363@ec2-54-80-122-11.compute-1.amazonaws.com:5432/d8gtv8aqkjjbqa'
     db.init_app(app)
 
     from .views import views
